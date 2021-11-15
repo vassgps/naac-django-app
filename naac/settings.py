95869334d9b07a894e55103c83aa19fbdc88b0e6
Anand VM <anandnambissan2@gmail.com>
2021-11-15 12:56:41 +0530

    Sub criterion 7.1.7ABCD Added

--- naac/settings.py
+++ naac/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$)v8srk36&$+$8%4&dsej=c4*vdn!9+m__)ry@w@zakihv+$$3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
